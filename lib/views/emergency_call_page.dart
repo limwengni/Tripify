c26211a25acd2fe0dfd6c605d1c4181c91d712be
westshine <weisiang0320030207@gmail.com>
2024-10-28 17:22:43 +0800

    let drawer and nav bar can work tgt

---
+++ lib/views/emergency_call_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EmergencyCallPage extends StatelessWidget {
+  const EmergencyCallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return 
+     const Center(
+        child: Text('Welcome to Tripify!'),
+    );
+  }
+}
---
+++ lib/widgets/tripify_navigation_bar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class TripifyNavBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onItemTapped;
+
+  const TripifyNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onItemTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final selectedColor =
+        Theme.of(context).colorScheme.onSurface.withOpacity(0.6);
+    final unselectedColor =
+        Theme.of(context).colorScheme.onSurface.withOpacity(0.6);
+
+    return BottomNavigationBar(
+      iconSize: 24,
+      selectedFontSize: 12.0,
+      unselectedFontSize: 12.0,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(
+            currentIndex == 0 ? Icons.home : Icons.home_outlined,
+            color: currentIndex == 0 ? selectedColor : unselectedColor,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            currentIndex == 1
+                ? Icons.shopping_cart
+                : Icons.shopping_cart_outlined,
+            color: currentIndex == 1 ? selectedColor : unselectedColor,
+          ),
+          label: 'Marketplace',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            currentIndex == 2 ? Icons.map : Icons.map_outlined,
+            color: currentIndex == 2 ? selectedColor : unselectedColor,
+          ),
+          label: 'Itinerary',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            currentIndex == 3 ? Icons.local_taxi : Icons.local_taxi_outlined,
+            color: currentIndex == 3 ? selectedColor : unselectedColor,
+          ),
+          label: 'Request',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            currentIndex > 3 ? Icons.more_rounded : Icons.more_outlined,
+            color: currentIndex > 3 ? selectedColor : unselectedColor,
+          ),
+          label: 'More',
+        ),
+      ],
+      currentIndex: currentIndex,
+      onTap: onItemTapped,
+      selectedItemColor: selectedColor,
+      unselectedItemColor: unselectedColor,
+      type: BottomNavigationBarType.fixed,
+    );
+  }
+}
