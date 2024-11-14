6493430a852bc448ddfea2e68fca7b1b6a443949
wengni <lim1227december@gmail.com>
2024-11-14 19:01:51 +0800

    post ui done, later integrate with firebase

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -75,6 +75,9 @@ class _ProfilePageState extends State<ProfilePage> {
             const SizedBox(height: 10.0), // Spacing below title
 
             // Grid for displaying user posts
+            _buildPostGrid(),
+
+            // Grid for displaying user posts
             // Expanded(
             //   child: FutureBuilder<List<Post>>(
             //     future: userProvider.fetchUserPosts(), // Replace with your actual method
@@ -104,156 +107,6 @@ class _ProfilePageState extends State<ProfilePage> {
             //     },
             //   ),
             // ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildShimmerPlaceholder() {
-    return Container(
-      padding: const EdgeInsets.only(top: 0.0, bottom: 16.0),
-      decoration: BoxDecoration(
-        border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // Row 1: Profile Picture and Username
-          Row(
-            children: [
-              // Profile Picture Shimmer
-              Shimmer.fromColors(
-                baseColor: Colors.grey.shade300,
-                highlightColor: Colors.grey.shade100,
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.grey.shade200,
-                ),
-              ),
-              const SizedBox(width: 16.0),
-              // Username and Join Date Shimmer
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Shimmer.fromColors(
-                      baseColor: Colors.grey.shade300,
-                      highlightColor: Colors.grey.shade100,
-                      child: Container(
-                        height: 20.0,
-                        width: 150,
-                        color: Colors.grey.shade300,
-                      ),
-                    ),
-                    const SizedBox(height: 4.0),
-                    Shimmer.fromColors(
-                      baseColor: Colors.grey.shade300,
-                      highlightColor: Colors.grey.shade100,
-                      child: Container(
-                        height: 16.0,
-                        width: 100,
-                        color: Colors.grey.shade300,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 16.0),
-
-          // Row 2: Bio Shimmer
-          Shimmer.fromColors(
-            baseColor: Colors.grey.shade300,
-            highlightColor: Colors.grey.shade100,
-            child: Container(
-              height: 16.0,
-              width: double.infinity,
-              color: Colors.grey.shade300,
-            ),
-          ),
-          const SizedBox(height: 8.0),
-          Shimmer.fromColors(
-            baseColor: Colors.grey.shade300,
-            highlightColor: Colors.grey.shade100,
-            child: Container(
-              height: 16.0,
-              width: double.infinity,
-              color: Colors.grey.shade300,
-            ),
-          ),
-          const SizedBox(height: 16.0),
-
-          // Row 3: Likes, Comments, and Saved Shimmer
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  _buildStatPlaceholder(),
-                  const SizedBox(width: 16.0),
-                  _buildStatPlaceholder(),
-                  const SizedBox(width: 16.0),
-                  _buildStatPlaceholder(),
-                ],
-              ),
-              // Edit Profile Button Shimmer
-              Shimmer.fromColors(
-                baseColor: Colors.grey.shade300,
-                highlightColor: Colors.grey.shade100,
-                child: Container(
-                  height: 40.0,
-                  width: 100.0,
-                  color: Colors.grey.shade300,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-// Create a shimmer effect for each stat (Likes, Comments, Saved)
-  Widget _buildStatPlaceholder() {
-    return Column(
-      children: [
-        Shimmer.fromColors(
-          baseColor: Colors.grey.shade300,
-          highlightColor: Colors.grey.shade100,
-          child: Container(
-            height: 16.0,
-            width: 40.0,
-            color: Colors.grey.shade300,
-          ),
-        ),
-        const SizedBox(height: 4.0),
-        Shimmer.fromColors(
-          baseColor: Colors.grey.shade300,
-          highlightColor: Colors.grey.shade100,
-          child: Container(
-            height: 14.0,
-            width: 30.0,
-            color: Colors.grey.shade300,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildPostUi(String postId) {
-    return Card(
-      elevation: 2,
-      child: Container(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            // Example content for the post
-            Image.network(
-                'https://example.com/post_image/$postId'), // Replace with your image URL
-            SizedBox(height: 8.0),
-            Text('Post Title for $postId'),
           ],
         ),
       ),
@@ -299,45 +152,6 @@ class _ProfilePageState extends State<ProfilePage> {
                     backgroundImage: imageProvider,
                   ),
                 ),
-                // FutureBuilder<String>(
-                //   future: _profileImageUrl,
-                //   builder: (context, snapshot) {
-                //     if (snapshot.connectionState == ConnectionState.waiting) {
-                //       return Shimmer.fromColors(
-                //         baseColor: Colors.grey.shade300,
-                //         highlightColor: Colors.grey.shade100,
-                //         child: CircleAvatar(
-                //           radius: 50,
-                //           backgroundColor: Colors.grey.shade200,
-                //         ),
-                //       );
-                //     } else if (snapshot.hasError) {
-                //       // Handle the error case
-                //       return CircleAvatar(
-                //         radius: 50,
-                //         backgroundColor: Colors.grey.shade200,
-                //         child: Icon(Icons.error), // Error icon
-                //       );
-                //     } else {
-                //       // Image URL loaded successfully
-                //       return CircleAvatar(
-                //         radius: 50,
-                //         backgroundImage:
-                //             CachedNetworkImageProvider(snapshot.data!),
-                //       );
-
-                //       // return ClipOval(
-                //       //     child: Image.network(
-                //       //   'https://firebasestorage.googleapis.com/v0/b/tripify-d8e12.appspot.com/o/AD9FpoxYM1XgY9h5JIV2QQaOouU2%2Fpfp%2Fpfp.jpg?alt=media&token=812a109f-05a2-4535-84ce-79666b652c60',
-                //       //   width:
-                //       //       60, // Set width and height to match the CircleAvatar size
-                //       //   height: 60,
-                //       //   fit: BoxFit
-                //       //       .cover, // Crop the image to fit inside the circle
-                //       // ));
-                //     }
-                //   },
-                // ),
                 const SizedBox(width: 16.0),
                 // Username and Join Date
                 Expanded(
@@ -501,5 +315,320 @@ class _ProfilePageState extends State<ProfilePage> {
     if (result == true) {
       _fetchUserData();
     }
+  }
+
+  Widget _buildShimmerPlaceholder() {
+    return Container(
+      padding: const EdgeInsets.only(top: 0.0, bottom: 16.0),
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.grey.shade300)),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Row 1: Profile Picture and Username
+          Row(
+            children: [
+              // Profile Picture Shimmer
+              Shimmer.fromColors(
+                baseColor: Colors.grey.shade300,
+                highlightColor: Colors.grey.shade100,
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.grey.shade200,
+                ),
+              ),
+              const SizedBox(width: 16.0),
+              // Username and Join Date Shimmer
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Shimmer.fromColors(
+                      baseColor: Colors.grey.shade300,
+                      highlightColor: Colors.grey.shade100,
+                      child: Container(
+                        height: 20.0,
+                        width: 150,
+                        color: Colors.grey.shade300,
+                      ),
+                    ),
+                    const SizedBox(height: 4.0),
+                    Shimmer.fromColors(
+                      baseColor: Colors.grey.shade300,
+                      highlightColor: Colors.grey.shade100,
+                      child: Container(
+                        height: 16.0,
+                        width: 100,
+                        color: Colors.grey.shade300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16.0),
+
+          // Row 2: Bio Shimmer
+          Shimmer.fromColors(
+            baseColor: Colors.grey.shade300,
+            highlightColor: Colors.grey.shade100,
+            child: Container(
+              height: 16.0,
+              width: double.infinity,
+              color: Colors.grey.shade300,
+            ),
+          ),
+          const SizedBox(height: 8.0),
+          Shimmer.fromColors(
+            baseColor: Colors.grey.shade300,
+            highlightColor: Colors.grey.shade100,
+            child: Container(
+              height: 16.0,
+              width: double.infinity,
+              color: Colors.grey.shade300,
+            ),
+          ),
+          const SizedBox(height: 16.0),
+
+          // Row 3: Likes, Comments, and Saved Shimmer
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  _buildStatPlaceholder(),
+                  const SizedBox(width: 16.0),
+                  _buildStatPlaceholder(),
+                  const SizedBox(width: 16.0),
+                  _buildStatPlaceholder(),
+                ],
+              ),
+              // Edit Profile Button Shimmer
+              Shimmer.fromColors(
+                baseColor: Colors.grey.shade300,
+                highlightColor: Colors.grey.shade100,
+                child: Container(
+                  height: 40.0,
+                  width: 100.0,
+                  color: Colors.grey.shade300,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+// Create a shimmer effect for each stat (Likes, Comments, Saved)
+  Widget _buildStatPlaceholder() {
+    return Column(
+      children: [
+        Shimmer.fromColors(
+          baseColor: Colors.grey.shade300,
+          highlightColor: Colors.grey.shade100,
+          child: Container(
+            height: 16.0,
+            width: 40.0,
+            color: Colors.grey.shade300,
+          ),
+        ),
+        const SizedBox(height: 4.0),
+        Shimmer.fromColors(
+          baseColor: Colors.grey.shade300,
+          highlightColor: Colors.grey.shade100,
+          child: Container(
+            height: 14.0,
+            width: 30.0,
+            color: Colors.grey.shade300,
+          ),
+        ),
+      ],
+    );
+  }
+
+// Create a shimmer effect for each posts
+  Widget _buildPostShimmer() {
+    return Card(
+      elevation: 2,
+      child: Container(
+        padding: EdgeInsets.all(0.0),
+        child: Column(
+          children: [
+            // Image Shimmer
+            Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.grey.shade100,
+              child: Container(
+                height: 180.0, // Same height as the actual image
+                width: double.infinity,
+                color: Colors.grey.shade300,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Title Shimmer
+                  Shimmer.fromColors(
+                    baseColor: Colors.grey.shade300,
+                    highlightColor: Colors.grey.shade100,
+                    child: Container(
+                      height: 16.0,
+                      width: 100.0, // Adjust width as per the title length
+                      color: Colors.grey.shade300,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox.shrink(), // For alignment
+                      Row(
+                        children: [
+                          // Like icon shimmer
+                          Shimmer.fromColors(
+                            baseColor: Colors.grey.shade300,
+                            highlightColor: Colors.grey.shade100,
+                            child: Icon(
+                              Icons.favorite_outline,
+                              color: Colors.grey.shade300,
+                              size: 16.0,
+                            ),
+                          ),
+                          SizedBox(width: 4.0),
+                          // Like count shimmer
+                          Shimmer.fromColors(
+                            baseColor: Colors.grey.shade300,
+                            highlightColor: Colors.grey.shade100,
+                            child: Container(
+                              height: 12.0,
+                              width:
+                                  40.0, // Adjust width for like count placeholder
+                              color: Colors.grey.shade300,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+// Grid for displaying post shimmers
+  Widget _buildPostShimmerGrid() {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.75,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+      ),
+      itemCount: 10, // Show 10 placeholders
+      itemBuilder: (context, index) {
+        return _buildPostShimmer(); // Display shimmer placeholders
+      },
+    );
+  }
+
+// Display shimmer grid until posts are loaded
+  Widget _buildPostGrid() {
+    bool isLoading = false; // Update this based on loading status
+
+    return isLoading
+        ? _buildPostShimmerGrid()
+        : GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.75,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+            ),
+            itemCount: 10, // Set this number based on actual post count
+            itemBuilder: (context, index) {
+              return _buildPostUi(); // Display actual post UI
+            },
+          );
+  }
+
+// Build Post UI
+  Widget _buildPostUi() {
+    return Card(
+        elevation: 2,
+        child: Container(
+          padding: EdgeInsets.all(0.0),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12.0),
+                  topRight: Radius.circular(12.0),
+                ),
+                child: CachedNetworkImage(
+                  imageUrl:
+                      'https://firebasestorage.googleapis.com/v0/b/tripify-d8e12.appspot.com/o/GdkQDokljsVMIkPLanvtFdoshDR2%2Fpfp%2F1731142797668_IMG-20241109-WA0003.jpg?alt=media&token=69c27493-5a8f-48e9-84a3-3967e64d27e4',
+                  placeholder: (context, url) => Shimmer.fromColors(
+                    baseColor: Colors.grey[300]!,
+                    highlightColor: Colors.grey[100]!,
+                    child: Container(
+                      color: Colors.white,
+                      height: 180.0, // Fixed height for the shimmer
+                      width: double.infinity,
+                    ),
+                  ),
+                  errorWidget: (context, url, error) =>
+                      Icon(Icons.error), // Icon on error
+                  fit: BoxFit.cover,
+                  height: 180.0, // Fixed height for the image
+                  width: double.infinity,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize
+                      .min, // Prevents the column from expanding too much
+                  children: [
+                    Text(
+                      'Post Title',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox.shrink(),
+                        Row(
+                          children: [
+                            Icon(Icons.favorite_outline, size: 16.0),
+                            SizedBox(width: 4.0),
+                            Text(
+                              '42', // Replace with actual like count if needed
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
